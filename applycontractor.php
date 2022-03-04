0e4b55b34296b702381466b531395f4cb46f15b3
William Sarawichitr <william.sarawichitr@gmail.com>
2022-03-03 21:03:39 -0600

    added some forms

---
+++ applycontractor.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <?php include('templates/header.php'); ?>
+        <title></title>
+    </head>
+    <body>
+        <form action="">
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"><br><br>
+            <label>Address:</label>
+            <input type="text"><br><br>
+            <label>City:</label>
+            <input type="text"><br><br>
+            <label>State:</label>
+            <input type="text"><br><br>
+            <label>Zip Code:</label>
+            <input type="text"><br><br>
+            <label>Phone Number:</label>
+            <input type="text"><br><br>
+            <label>Email Address:</label>
+            <input type="text"><br><br>
+            <label>Driver's License Number:</label>
+            <input type="text"><br><br>
+            <label>Social Security Number/Proof EIN Number:</label>
+            <input type="text"><br><br>
+
+            <input type="submit" value="Submit">
+        </form> 
+    </body>
+
+    <?php include('templates/footer.php'); ?>
+
+
+
+</html>
---
+++ applycustomer.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <?php include('templates/header.php'); ?>
+        <title></title>
+    </head>
+    <body>
+        <form action="">
+            <label>Company Name:</label>
+            <input type="text"><br><br>
+            <label>Address:</label>
+            <input type="text"><br><br>
+            <label>City:</label>
+            <input type="text"><br><br>
+            <label>State:</label>
+            <input type="text"><br><br>
+            <label>Zip Code:</label>
+            <input type="text"><br><br>
+            <label>Phone Number:</label>
+            <input type="text"><br><br>
+            <label>Email Address:</label>
+            <input type="text"><br><br>
+            <label>Billing Info:</label>
+            <input type="text"><br><br>
+            <label>Credit References:</label>
+            <input type="text"><br><br>
+
+
+            <input type="submit" value="Submit">
+        </form> 
+    </body>
+
+    <?php include('templates/footer.php'); ?>
+
+
+
+</html>
