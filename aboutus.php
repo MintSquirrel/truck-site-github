1d17b99838c5a81757de871c189860caecbe06f0
johnnyvx7 <johnvillasenor7@gmail.com>
2022-04-08 22:04:51 -0500

    About us updated

--- aboutus.php
+++ aboutus.php
@@ -24,12 +24,23 @@
         About Us
     </div>
     <section>
-        <p>The Roadstar Trucking Company is an old-school LLC. helps companies attain operational excellence by providing transportation <br>
-            solutions tailored for their individual needs. Our job is to provide the most cost-efficient transportation while offering the highest quality<br>
-            service available. Our years of experience have taught us that “a one size fits all” type of transportation does not work in this complex <br>
-            and ever changing industry. Companies need real-time information from their suppliers to their end-user, which helps them reduce<br>
-            lead times and optimize their inventory while reducing transportation costs. The reason for our continued growth and success is due <br>
-            to making our customers satisfaction our number one goal at Road Star. </p>
+        <h1>We are Roadstar Trucking Services, LLC.</h1>
+        <h2>
+            Location: 5326 W. Ledbetter Dr. Dallas, TX 75236
+            Office Number: 469-291-7755
+            Fax Number: 469-291-7898
+        </h2>
+        <p> Roadstar Trucking was started in 2001 by Daniel Tarango. Initially starting with one 20 yard end dump truck. <br>
+            We contract work for independent contractors. We have trucks specializing in contruction and demolition. On average, <br>
+            about 60-10 trucks go out daily. We run 10 yard tandems, 20 yard end dumps, and 60 yard demolition trailers. We also <br>
+            have access to belly dumps and tri-axle trailers. Our job is to provide the most cost-efficient transportation while <br>
+            offering the highest quality service available. We haul: dirt, gravel, sand, rock, concrete, asphalt, and<br>
+            anything that goes in and out of a contruction site. We also help companies attain operational excellence by<br>
+            providing transportation solutions tailored for their individual needs. Our years of experience have taught <br>
+            us that “a one size fits all” type of transportation does not work in this complex and ever changing industry. Companies<br>
+            need real-time information from their suppliers to their end-user, which helps them reduce lead times and optimize<br>
+            their inventory while reducing transportation costs. The reason for our continued growth and success is due to making<br>
+            our customers satisfaction our number one goal at Road Star. </p>
     </section>
     
     </body>
