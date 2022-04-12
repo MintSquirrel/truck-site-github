65a035be89908571ee2e70bf0d05156f3111f655
Chadrick Davis <57962060+MintSquirrel@users.noreply.github.com>
2022-04-12 12:54:14 -0500

    email proof of concept on email.php

--- email.php
+++ email.php
@@ -7,24 +7,36 @@
    <body>
 
       <?php
-         $to = "dragonsquirrelcd@gmail.com";
-         $subject = "This is subject";
+      use PHPMailer\PHPMailer\PHPMailer;
+      use PHPMailer\PHPMailer\Exception;
+      require 'vendor/phpmailer/phpmailer/src/Exception.php';
+      require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+      require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+      $mail = new PHPMailer();
+      $mail->IsSMTP();
+      $mail->Mailer = "smtp";
+      $mail->SMTPDebug  = 1;
+      $mail->SMTPAuth   = TRUE;
+      $mail->SMTPSecure = "tls";
+      $mail->Port       = 587;
+      $mail->Host       = "smtp.gmail.com";
+      $mail->Username   = "roadrunnersemaildemount@gmail.com";
+      $mail->Password   = "Roadrunners!1";
+      $mail->IsHTML(true);
+      $mail->AddAddress("dragonsquirrelcd@gmail.com", "recipient-name");
+      $mail->SetFrom("roadrunnersemaildemount@gmail.com", "Roadrunners");
+      $mail->AddReplyTo("roadrunnersemaildemount@gmail.com", "do-not-reply");
+      $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+      $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
 
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
+      $mail->MsgHTML($content);
+      if(!$mail->Send()) {
+        echo "Error while sending Email.";
+        var_dump($mail);
+      } else {
+        echo "Email sent successfully";
+      }
 
-         $header = "From:<abc@somedomain.com> \r\n";
-         $header .= "Cc:<afgh@somedomain.com> \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-
-         $retval = mail ($to,$subject,$message,$header);
-
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
       ?>
 
    </body>
