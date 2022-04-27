a26613f9eb071d585dceb02bfa226e931184a42e
Chadrick Davis <57962060+MintSquirrel@users.noreply.github.com>
2022-04-27 15:03:05 -0500

    Updated banner

--- templates/header.php
+++ templates/header.php
@@ -18,26 +18,27 @@
 
 <!-- Body -->
 <body class="grey lighten-4">
-    <nav class="white z-depth-0">
-        <div class="container">
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="index.php" class="btn brand z-depth-0">Home</a></li>
-                <li><a href="applycustomer.php" class="btn brand z-depth-0">Customers</a></li>
-                <li><a href="applycontractor.php" class="btn brand z-depth-0">Contractor</a></li>
-                <li><a href="aboutus.php" class="btn brand z-depth-0">About Us</a></li>
-                <li><a href="privacypolicy.php" class="btn brand z-depth-0">Privacy Policy</a></li>
+
+  <nav>
+      <div class="nav-wrapper">
+        <ul class="right hide-on-med-and-down">
+                <li><a href="index.php" >Home</a></li>
+                <li><a href="applycustomer.php" >Customers</a></li>
+                <li><a href="applycontractor.php" >Contractor</a></li>
+                <li><a href="aboutus.php" >About Us</a></li>
+                <li><a href="privacypolicy.php" >Privacy Policy</a></li>
                 <?php
                     if(isset($_SESSION["username"])){
-                        echo "<li><a href='profile.php' class='btn brand z-depth-0'>Profile</a></li>";
+                        echo "<li><a href='profile.php' >Profile</a></li>";
                         if($_SESSION["account"] === "Admin"){
-                            echo "<li><a href='admin_db.php' class='btn brand z-depth-0'>View Data</a></li>";
-                            echo "<li><a href='admin_create.php' class='btn brand z-depth-0'>Create Account</a></li>";
+                            echo "<li><a href='admin_db.php' >View Data</a></li>";
+                            echo "<li><a href='admin_create.php' >Create Account</a></li>";
                         }
-                        echo "<li><a href='config/logout-run.php' class='btn brand z-depth-0'>Log out</a></li>";
+                        echo "<li><a href='config/logout-run.php' >Log out</a></li>";
                     }
                     else{
-                        echo "<li><a href='register.php' class='btn brand z-depth-0'>Register</a></li>";
-                        echo "<li><a href='login.php' class='btn brand z-depth-0'>Login</a></li>";
+                        echo "<li><a href='register.php' >Register</a></li>";
+                        echo "<li><a href='login.php' >Login</a></li>";
                     }
                 ?>
             </ul>
